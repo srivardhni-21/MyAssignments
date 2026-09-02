ba868353ce0b53023b7de70fe9d56e0f0d5c1878
srivardhni-21 <ksrivardhni@gmail.com>
2026-09-02 21:24:32 +0530

    Add files via upload
    day3

---
+++ week1day3/usinglet.js
@@ -0,0 +1,13 @@
+const browserVersion = "Chrome";
+
+function getBrowserVersion() {
+if (browserVersion === "Chrome") {
+let browserVersion = "Edge";
+console.log(browserVersion);
+}
+
+console.log(browserVersion);
+}
+
+getBrowserVersion();
+ 
---
+++ week1day3/varvsletvsconst.js
@@ -0,0 +1,11 @@
+const browserVersion = "Chrome";
+
+function getBrowserVersion() {
+if (browserVersion === "Chrome") {
+var browserVersion = "Edge";
+}
+
+console.log(browserVersion);
+}
+
+getBrowserVersion();
