66d6beb3fe7fce5c8a0915cc213a0585ba10aae7
srivardhni-21 <ksrivardhni@gmail.com>
2026-09-02 13:47:53 +0530

    Add files via upload
    day2

---
+++ week1day2/JavaScriptBasic.js
@@ -0,0 +1,20 @@
+var firstName = "Srivardhni";
+var companyName = "Testleaf";
+var mobileNumber = 9876543210;
+var isAutomation = true;
+var hasPlaywright;
+
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(companyName);
+console.log(typeof companyName);
+
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
